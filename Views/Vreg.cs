aa5da7f49dc257f7672101d91c49ad46e6ceb907
I Wayan Awi Luistika Cen <120024043+awi02@users.noreply.github.com>
2023-04-04 20:27:58 +0700

    Add files via upload

---
+++ Views/Vreg.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Connection.Models;
+namespace Connection.Views
+{
+    internal class Vreg
+    {
+        public void GetAll(List<Region> regions)
+        {
+            foreach (var region in regions)
+            {
+                Console.WriteLine("=================");
+                Console.WriteLine("Id: " + region.Id);
+                Console.WriteLine("Name: " + region.Name);
+            }
+        }
+        public void GetById(Region region)
+        {
+            Console.WriteLine("=================");
+            Console.WriteLine("Id: " + region.Id);
+            Console.WriteLine("Name: " + region.Name);
+        }
+        public void Success(string message)
+        {
+            Console.WriteLine($"Data has been {message}");
+        }
+
+        public void Failure(string message)
+        {
+            Console.WriteLine($"Data has not been {message}");
+        }
+
+        public void DataNotFound()
+        {
+            Console.WriteLine("Data Not Found!");
+        }
+    }
+}
